2a137ea1332e02150b6c5a71f77d0978deec6df0
Lakshmi-02-05 <lak.csgirl@gmail.com>
2026-07-08 16:57:14 +0000

    Basic Test for quick rev day 2

---
+++ 09-PracticeQsts/Basictst2.js
@@ -0,0 +1,209 @@
+// 1. Which keyword can be reassigned?
+
+// Answer: B. let
+
+// 2. What is the datatype of typeof null?
+
+// Answer: "object"
+
+// 3. Predict the Output
+// let x = "10";
+// let y = 5;
+
+// console.log(x + y);
+
+// Answer: "105"
+
+// 4. Predict the Output
+// console.log(10 - "3");
+
+// Answer: 7
+
+// 5. Predict the Output
+// console.log(5 == "5");
+
+// Answer: true
+
+// 6. Predict the Output
+// console.log(5 === "5");
+
+// Answer: false
+
+// 7. Predict the Output
+// console.log(Boolean(""));
+
+// Answer: false
+
+// 8. Predict the Output
+// console.log(Boolean(" "));
+
+// Answer: true
+
+// 9. Predict the Output
+// console.log(0 == false);
+
+// Answer: true
+
+// 10. Predict the Output
+// let a = 8;
+
+// a += 5;
+
+// console.log(a);
+
+// Answer: 13
+
+// PART 2
+// Q1
+// let age = 15;
+
+// if(age >=18){
+// console.log("Adult");
+// }
+// else{
+// console.log("Minor");
+// }
+
+// Answer: Minor
+
+// Q2
+// let marks = 80;
+
+// if(marks >=35){
+// console.log("Pass");
+// }
+// else{
+// console.log("Fail");
+// }
+
+// Answer: Pass
+
+// Q3
+// let num = 0;
+
+// if(num){
+// console.log("Yes");
+// }
+// else{
+// console.log("No");
+// }
+
+// Answer: No
+
+// Q4
+// console.log(10 > 5 && 4 > 8);
+
+// Answer: false
+
+// Q5
+// console.log(10 > 5 || 4 > 8);
+
+// Answer: true
+
+// PART 3
+// Output?
+// let x = 1;
+
+// switch(x){
+
+// case 1:
+// console.log("A");
+
+// case 2:
+// console.log("B");
+
+// default:
+// console.log("C");
+
+// }
+
+// Answer:
+
+// A
+// B
+// C
+// PART 4
+// Question: How many times does this execute?
+// let i = 10;
+
+// do{
+
+// console.log(i);
+
+// i++;
+
+// }
+// while(i<=5);
+
+// Answer: 1 time
+
+// Q1
+// for(let i=1;i<=3;i++){
+
+// console.log(i);
+
+// }
+
+// Answer:
+
+// 1
+// 2
+// 3
+// Q2
+// for(let i=3;i>=1;i--){
+
+// console.log(i);
+
+// }
+
+// Answer:
+
+// 3
+// 2
+// 1
+// Q3
+// let i = 1;
+
+// while(i<4){
+
+// console.log(i);
+
+// i++;
+
+// }
+
+// Answer:
+
+// 1
+// 2
+// 3
+// Q4
+// let i = 5;
+
+// do{
+
+// console.log(i);
+
+// i++;
+
+// }
+// while(i<5);
+
+// Answer:
+
+// 5
+// Q5
+// for(let i=1;i<=5;i++){
+
+// if(i==3){
+// break;
+// }
+
+// console.log(i);
+
+// }
+
+// Answer:
+
+// 1
+// 2
